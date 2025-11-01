08af8a8bb542812e5e94ffaad640da67e6515ac5
EdisonSanchezJim <115053765+EdisonSanchezJim@users.noreply.github.com>
2025-10-31 22:50:19 -0500

    Update Application.java

--- Taller2/distributed-kv-store/bakend/src/main/java/edu/escuelaing/distributedpatterns/Application.java
+++ Taller2/distributed-kv-store/bakend/src/main/java/edu/escuelaing/distributedpatterns/Application.java
@@ -2,7 +2,6 @@ package edu.escuelaing.distributedpatterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
